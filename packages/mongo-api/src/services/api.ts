c4295bf92c7a3641f942f98739a18edb5d613688
Jerome Nelson <jerome.nelson@skelia.com>
2020-10-04 16:20:57 +0200

    fix(api): addition of authorization header to api

--- packages/mongo-api/src/services/api.ts
+++ packages/mongo-api/src/services/api.ts
@@ -10,7 +10,7 @@ export function server<T>(routes: (app: Application) => void) {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
       next();
     });
     routes(app);
