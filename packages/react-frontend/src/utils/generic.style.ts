1bede8a873b95690c8a3ad452080aefba520b4b8
Jerome Nelson <jerome.nelson@skelia.com>
2020-12-05 19:12:31 +0100

    fix: overriding of global styles

--- packages/react-frontend/src/utils/generic.style.ts
+++ packages/react-frontend/src/utils/generic.style.ts
@@ -3,7 +3,7 @@ import { createStyles, makeStyles, Theme } from "@material-ui/core";
 export const useGenericStyle = makeStyles((theme: Theme) =>
     createStyles({
         userFields: {
-            borderRadius: `${theme.spacing(3)}px`,
+            borderRadius: `${theme.spacing(3)}px !important`,
             marginTop: `${theme.spacing(3)}px`,
         },
        actionButton: {
